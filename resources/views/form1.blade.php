b48d3428c3932d098d46b9f17ef0078ad899d4de
OlaFouadKotb <engolakotb154@gmail.com>
2024-04-29 02:13:21 +0200

    first commit

--- resources/views/form1.blade.php
+++ resources/views/form1.blade.php
@@ -5,11 +5,11 @@
 <h2>HTML Forms</h2>
 
 <form action="{{ route('receiveform1')}}" method="POST">
-  
+@csrf
   <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value="OlA"><br>
+  <input type="text" id="fname" name="fname" value=""><br>
   <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="FouaD"><br><br>
+  <input type="text" id="lname" name="lname" value=""><br><br>
   <input type="submit" value="Submit">
 </form> 
 
